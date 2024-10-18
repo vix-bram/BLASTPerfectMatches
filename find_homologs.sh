50ee1e6ceefdb59adb010b67d5ac8ceb0cf2bf3a
Vikram Kaushik <78250803+vix-bram@users.noreply.github.com>
2024-10-17 23:05:36 -0400

    trivial update find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -3,7 +3,6 @@
 # usage: find_perfect_matches.sh <query file> <subjectfile> <output file>
 
 tblastn -query $1 -subject $2 -outfmt '6 std qlen' -task tblastn | awk '$3>=30 && $4>=($13)*0.9' > $3
-
 wc -l $3
 
 
