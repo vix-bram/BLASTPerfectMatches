ecb41e25b8df8d782f7caebd7492affee3a425cd
Vikram Kaushik <78250803+vix-bram@users.noreply.github.com>
2024-10-18 18:51:02 -0400

    Delete find_perfect_matches.sh

--- find_perfect_matches.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-# usage: find_perfect_matches.sh <query file> <subjectfile> <output file>
-
-blastn -query $1 -subject $2 -outfmt '6 std qlen' -task blastn-short | awk '$3==100 && $4==$13' > $3
-wc -l $3
